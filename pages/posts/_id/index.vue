a044f4c89dae55fa0fa89126759ec0c951d653c0
Leonardo <lvneves2010@gmail.com>
2020-05-16 10:52:53 -0300

    main routes

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="single-post-page">
+      <section class="post">
+          <h1 class="post-title">Title for the post</h1>
+          <div class="post-details">
+              <div class="post-detail">Last Updated</div>
+              <div class="post-detail">Writen By:</div>
+          </div>
+          <p class="post-content">Content of the Post</p>
+      </section>
+      <section class="post-feedback">
+          <p>Let your comment here <a href="mailto: lvneves2010@gmail.com">lvneves2010@gmail.com</a> </p>
+      </section>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.single-post-page {
+  padding: 30px;
+  text-align: center;
+  box-sizing: border-box;
+}
+
+.post {
+  width: 100%;
+}
+
+@media (min-width: 768px) {
+  .post {
+    width: 600px;
+    margin: auto;
+  }
+}
+
+.post-title {
+  margin: 0;
+}
+
+.post-details {
+  padding: 10px;
+  box-sizing: border-box;
+  border-bottom: 3px solid #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+@media (min-width: 768px) {
+  .post-details {
+    flex-direction: row;
+  }
+}
+
+.post-detail {
+  color: rgb(88, 88, 88);
+  margin: 0 10px;
+}
+
+.post-feedback a {
+  color: red;
+  text-decoration: none;
+}
+
+.post-feedback a:hover,
+.post-feedback a:active {
+  color: salmon;
+}
+
+</style>
